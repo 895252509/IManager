3a5dbebe89cc66dca66f2495d2e43ef5231b8571
zx <895252509@qq.com>
2018-06-21 11:46:57 +0800

    更换绘图库

--- renderer-process/index.js
+++ renderer-process/index.js
@@ -5,27 +5,35 @@
 
 import MindMap from '../worker/mindmap/mindmap.js';
 
+const {fabric} = require('fabric');
+
 const $ = require('jquery');
 
 const mindmapDom = $('#main')[0];
-const mindMap = new MindMap(mindmapDom);
-$(mindMap.dom);
+//const mindMap = new MindMap(mindmapDom);
+
 $(window).on('resize', (e) => {
-  e.preventDefault();
+  // e.preventDefault();
   const width = window.innerWidth - 24;
   const height = window.innerHeight - 100;
-  /* $(mindmapDom).prop('width', `${width}px`);
-  $(mindmapDom).prop('height', `${height}px`); */
-  /* mindmapDom.style.width = width;
-  mindmapDom.style.height = height;
-  $(mindmapDom).resize(); */
+/*
   mindMap.resize({
     width,
     height,
   });
+*/
 });
 
 $(() => {
   $(window).trigger('resize');
+  var canvas = new fabric.Canvas('main');
+  var red = new fabric.Rect({
+    top: 0, left: 0, width: 80, height: 50, fill: 'red' });
+  var blue = new fabric.Rect({
+    top: 0, left: 100, width: 50, height: 70, fill: 'blue' });
+  var green = new fabric.Rect({
+    top: 100, left: 100, width: 60, height: 60, fill: 'green' });
+  fabric.Object.prototype.transparentCorners = true;
+  canvas.add(red, blue, green);
 });
 
